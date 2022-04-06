b47aa3540ce828fc557e54e2829825e1a18e3ca5
alannarisse <angrypirate@gmail.com>
2022-04-05 18:48:09 -0700

    added second jquery practice

---
+++ jquery-practice2/script.js
@@ -0,0 +1,14 @@
+// $('.animal-pic').css('border', '2px dotted black').css('padding', '40px')
+
+$('.one').click(function () {
+  $('.animal-pic').attr('src', 'images/hans-jurgen-mager-qQWV91TTBrE-unsplash.jpg')
+})
+$('.two').click(function () {
+  $('.animal-pic').attr('src', 'images/ray-hennessy-xUUZcpQlqpM-unsplash.jpg')
+})
+$('.three').click(function () {
+  $('.animal-pic').attr('src', 'images/sid-balachandran-_9a-3NO5KJE-unsplash.jpg')
+})
+$('.four').click(function () {
+  $('.animal-pic').attr('src', 'images/gwen-weustink-I3C1sSXj1i8-unsplash.jpg')
+})
